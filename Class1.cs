6fb424fa693bf9f9330532773b0963e247836af0
fraudulenteconomics <72472054+fraudulenteconomics@users.noreply.github.com>
2020-10-07 10:17:46 -0400

    Testing
    Initial Test

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+
+namespace FrauDecon
+{
+    [StaticConstructorOnStartup]
+    internal static class HarmonyInit
+    {
+        static HarmonyInit()
+        {
+            Log.Message("Loaded ReloadCompMod");
+            new Harmony("ReloadComp.Mod").PatchAll();
+        }
+    }
+    [HarmonyPatch(typeof(ReloadableUtility), "FindSomeReloadableComponent")]
+        static class Patch_ReloadableUtility_FindSomeReloadableComponent
+        {
+            static void Postfix(ref Pawn pawn, bool allowForcedReload, ref CompReloadable __result)
+            {
+                CompReloadable compReloadable = pawn.equipment.Primary.TryGetComp<CompReloadable>();
+                if (compReloadable != null && compReloadable.NeedsReload(allowForcedReload))
+                {
+                Log.Message("reloadable weapon");
+                __result = compReloadable;
+                }
+            Log.Message("not reloadable weapon");
+            __result = null;
+            }
+        }
+    }
---
+++ ReloadableWeaponsComp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+
+namespace FrauDecon
+{
+    [HarmonyPatch(typeof(ReloadableUtility), "FindSomeReloadableComponent")]
+    static class Patch_ReloadableUtility_FindSomeReloadableComponent
+    {
+        static void Postfix(ref Pawn pawn, bool allowForcedReload, CompReloadable __result)
+        {
+            CompReloadable compReloadable = pawn.equipment.Primary.TryGetComp<CompReloadable>();
+            if (compReloadable != null && compReloadable.NeedsReload(allowForcedReload))
+            {
+                __result = compReloadable;
+            }
+            __result = null;
+        }
+    }
+}
